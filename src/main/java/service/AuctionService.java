9946477138e2a4efd4ec5779ddf434239ee3c5e3
Maciek <macardorpl@gmail.com>
2021-06-10 15:24:44 +0200

    AuctionService template

---
+++ src/main/java/service/AuctionService.java
@@ -0,0 +1,22 @@
+package service;
+
+import model.Auction;
+
+public class AuctionService {
+    //TODO
+    public void createAuction(Auction auction){
+
+    }
+    //TODO
+    public Auction getAuction(int id){
+
+    }
+    //TODO
+    public void updateAuction(Auction auction){
+
+    }
+    //TODO
+    public void deleteAuction(int id){
+        
+    }
+}
