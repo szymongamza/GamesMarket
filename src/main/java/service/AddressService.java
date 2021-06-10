81c13d42de813a7c04e30ea49c681273be8fe263
Maciek <macardorpl@gmail.com>
2021-06-10 15:31:12 +0200

    AddressService template

---
+++ src/main/java/service/AddressService.java
@@ -0,0 +1,22 @@
+package service;
+
+import model.Address;
+
+public class AddressService {
+    //TODO
+    public void createAddress(Address address){
+
+    }
+    //TODO
+    public Address getAddress(int id){
+
+    }
+    //TODO
+    public void updateAddress(Address address){
+
+    }
+    //TODO
+    public void deleteAddress(int id){
+        
+    }
+}
