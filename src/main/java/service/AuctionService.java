c69e7df248f9be927b48c14b9a11d1c990eb20f9
Maciek <macardorpl@gmail.com>
2021-06-10 15:29:02 +0200

    CustomerService template

--- src/main/java/service/AuctionService.java
+++ src/main/java/service/AuctionService.java
@@ -17,6 +17,6 @@ public class AuctionService {
     }
     //TODO
     public void deleteAuction(int id){
-        
+
     }
 }
---
+++ src/main/java/service/CustomerService.java
@@ -0,0 +1,32 @@
+package service;
+
+import model.Customer;
+
+import java.util.List;
+
+public class CustomerService {
+    //TODO
+    public void createCustomer(Customer customer){
+
+    }
+    //TODO
+    public Customer getCustomer(int id){
+
+    }
+    //TODO
+    public void updateCustomer(Customer customer){
+
+    }
+    //TODO
+    public void deleteCustomer(int id){
+
+    }
+    //TODO
+    public void getCustomerByEmail(String email){
+
+    }
+    //TODO
+    public List<Customer> searchCustomers(String data){
+
+    }
+}
