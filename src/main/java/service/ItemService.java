1e0612a01b04f70d04a57807add3cbd1307e382e
Maciek <macardorpl@gmail.com>
2021-06-10 15:16:33 +0200

    ItemService template

---
+++ src/main/java/service/ItemService.java
@@ -0,0 +1,28 @@
+package service;
+
+import model.Item;
+
+import java.util.List;
+
+public class ItemService {
+    //TODO
+    public void createItem(Item item){
+
+    }
+    //TODO
+    public Item getItem(int id){
+
+    }
+    //TODO
+    public void updateItem(Item item){
+        
+    }
+    //TODO
+    public void deleteItem(int id){
+
+    }
+    //TODO
+    public List<Item> searchItems(String name){
+
+    }
+}
